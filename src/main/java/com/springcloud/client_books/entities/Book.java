d432c3dff0584fa19653b30fd98fce1596039b32
k-wasilewski <k.k.wasilewski@gmail.com>
2020-04-01 13:11:25 +0200

    session config (passing auth)

--- src/main/java/com/springcloud/client_books/entities/Book.java
+++ src/main/java/com/springcloud/client_books/entities/Book.java
@@ -11,10 +11,33 @@ public class Book {
     private String title;
     private String author;
 
+    /*private String timestamp;
+    private String message;
+    private int status;
+    private String error;*/
+
+    public Book() {}
+
+    public Book(String titile, String author) {
+        this.title=titile;
+        this.author=author;
+    }
+
+    /*}
+    public void setTimestamp(String timestamp) {this.timestamp=timestamp;}
+    public String getMessage() {return this.message;}
+    public void setMessage(String message) {this.message=message;}
+    public int getStatus() {return this.status;}
+    public void setStatus(int status) {this.status=status;}
+    public String getError() {return this.error;}
+    public void setError(String error) {this.error=error;}*/
+
     public long getId() {return this.id;}
     public void setId(long id) {this.id=id;}
     public String getTitle() {return this.title;}
     public void setTitle(String title) {this.title=title;}
     public String getAuthor() {return this.author;}
     public void setAuthor(String author) {this.author=author;}
+
+    public String toString() {return "id="+this.id+"&title="+this.title+"&author="+this.author;}
 }
--- src/test/java/com/springcloud/client_books/ClientBooksApplicationTests.java
+++ src/test/java/com/springcloud/client_books/ClientBooksApplicationTests.java
@@ -1,13 +1,64 @@
 package com.springcloud.client_books;
 
+import com.springcloud.client_books.entities.Book;
+import io.restassured.RestAssured;
+import io.restassured.authentication.FormAuthConfig;
+import io.restassured.config.RedirectConfig;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 @SpringBootTest
 class ClientBooksApplicationTests {
 
+    private final String ROOT_URI = "http://localhost:8084";
+    private FormAuthConfig formConfig
+            = new FormAuthConfig("/login", "username", "password");
+
+    @Before
+    public void setup() {
+        RestAssured.config = RestAssured.config().redirect(
+                RedirectConfig.redirectConfig().followRedirects(false));
+    }
+
     @Test
-    void contextLoads() {
+    public void whenGetAllBooks_thenSuccess() {
+        Response response = RestAssured.get(ROOT_URI + "/books");
+
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+        Assert.assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void whenAccessProtectedResourceWithoutLogin_thenRedirectToLogin() {
+        Response response = RestAssured.get(ROOT_URI + "/books/1");
+
+        Assert.assertEquals(HttpStatus.FOUND.value(), response.getStatusCode());
+        Assert.assertEquals("http://localhost:8084/login",
+                response.getHeader("Location"));
+    }
+
+    @Test
+    public void whenAddNewBook_thenSuccess() {
+        RestAssured.defaultParser = Parser.JSON;
+
+        Book book = new Book("How to spring cloud", "Baeldung");
+        Response bookResponse = RestAssured.given().auth()
+                .form("admin", "admin", formConfig).and()
+                .contentType(ContentType.JSON)
+                .body(book)
+                .post(ROOT_URI + "/books");
+        Book result = bookResponse.as(Book.class);
+        System.out.println(result);
+
+        Assert.assertEquals(HttpStatus.OK.value(), bookResponse.getStatusCode());
+        Assert.assertEquals(book.getAuthor(), result.getAuthor());
+        Assert.assertEquals(book.getTitle(), result.getTitle());
     }
 
 }
