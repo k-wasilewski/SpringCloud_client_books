8d1c026db86541f18655a77839ed569a30044454
k-wasilewski <k.k.wasilewski@gmail.com>
2020-04-05 17:28:54 +0200

    enabling discovery

--- src/main/java/com/springcloud/client_books/ClientBooksApplication.java
+++ src/main/java/com/springcloud/client_books/ClientBooksApplication.java
@@ -3,6 +3,7 @@ package com.springcloud.client_books;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
