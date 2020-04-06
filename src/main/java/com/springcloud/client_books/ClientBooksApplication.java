362d12f29a8625524cc178a652beb3f9a741d4a9
k-wasilewski <k.k.wasilewski@gmail.com>
2020-04-06 10:00:59 +0200

    accessing via discovery server

--- src/main/java/com/springcloud/client_books/ClientBooksApplication.java
+++ src/main/java/com/springcloud/client_books/ClientBooksApplication.java
@@ -2,12 +2,13 @@ package com.springcloud.client_books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableJpaRepositories("com.springcloud.client_books.repositories")
 public class ClientBooksApplication {
 
